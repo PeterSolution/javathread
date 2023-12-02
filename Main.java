0bfd48f64239a796247ffd8a22076091e45d90fd
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2023-12-02 12:48:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,37 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+//        JFrame frame = new JFrame("Prosty interfejs graficzny");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(400, 300);
+//
+//        // Ustalenie BorderLayout dla głównego kontenera
+//        frame.setLayout(new BorderLayout(0, 0)); // Ustawienie marginesów na 0
+//
+//        // Dodanie dwóch labeli na górze z odstępem 10 pikseli
+//        JPanel topPanel = new JPanel();
+//        topPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10)); // Ustawienie odstępów
+//        JLabel label1 = new JLabel("Pierwszy Label");
+//        JLabel label2 = new JLabel("Drugi Label");
+//        topPanel.add(label1);
+//        topPanel.add(label2);
+//        frame.add(topPanel, BorderLayout.NORTH);
+//
+//        // Dodanie pola tekstowego na środku ekranu
+//        JTextField textField = new JTextField(20);
+//        textField.setHorizontalAlignment(JTextField.CENTER); // Wyśrodkowanie tekstu
+//        frame.add(textField, BorderLayout.CENTER);
+//
+//        // Dodanie przycisku na dole ekranu z odstępem 20 pikseli
+//        JButton button = new JButton("Dodaj tekst");
+//        JPanel bottomPanel = new JPanel();
+//        bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20)); // Ustawienie odstępów
+//        bottomPanel.add(button);
+//        frame.add(bottomPanel, BorderLayout.SOUTH);
+//
+//        frame.setVisible(true);
+
+
         String stingip="";
         InetAddress localip;
         try {
@@ -20,35 +51,54 @@ public class Main {
         stingip= String.valueOf(localip);
         int substringip=stingip.lastIndexOf('/')+1;
         String ips=stingip.substring(substringip,stingip.length());
+
         JFrame frame=new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label=new JLabel("IP to: ");
-        frame.getContentPane().add(label, BorderLayout.NORTH);
-        JLabel labelip=new JLabel(ips);
-        FlowLayout layout=new FlowLayout(10);
-        frame.setLayout(layout);
+        frame.setSize(600, 600);
+        JPanel mp=new JPanel();
+        mp.setLayout(new BoxLayout(mp,BoxLayout.Y_AXIS));
 
-        JButton b1=new JButton("Modificate text");
-        JButton b2=new JButton("Save text");
-        frame.add(b1);
-        frame.add(b2);
-        frame.add(labelip);
+        JPanel top = new JPanel();
+        top.setLayout(new FlowLayout(FlowLayout.LEFT));
+        JLabel label = new JLabel("IP to: ");
+        top.add(label);
+        JLabel labelip = new JLabel(ips);
+        top.add(labelip);
+        mp.add(top);
+
+        JPanel mid=new JPanel();
+        mid.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JTextArea area=new JTextArea();
+        area.setPreferredSize(new Dimension(300,300));
+        mid.add(area);
+        mp.add(mid);
+
+        JPanel bot = new JPanel();
+        JButton b1 = new JButton("Modificate text");
+        JButton b2 = new JButton("Save text");
+        JPanel botpanel = new JPanel();
+        botpanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        botpanel.add(b1);
+        botpanel.add(b2);
+        mp.add(botpanel);
+
+        frame.add(mp);
+        frame.pack();
         frame.setVisible(true);
 
-        frame.setSize(400,500);
 
         ExecutorService writter= Executors.newFixedThreadPool(2);
         ExecutorService reader=Executors.newFixedThreadPool(2);
         ksiazka ksiazka1=new ksiazka();
 
-        int port=2234;
-
-        try {
-            Socket s1=new Socket(ips,port);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        int port=2234;
+//
+//        try {
+//            Socket s1=new Socket(ips,port);
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
 
 //        writter.submit(() ->{
