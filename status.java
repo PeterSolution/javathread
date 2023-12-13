4502d2e8eff6f6d36c2bba93e5fe1f575d065f02
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2023-12-13 22:20:22 +0100

    Add files via upload

---
+++ status.java
@@ -0,0 +1,15 @@
+public class status {
+    String status;
+    public status(){
+        this.status="free";
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
